425dbe73e88642c9c33e87e9499f40b138a7f83a
unknown <isidromanuel39@gmail.com>
2023-01-14 11:52:59 +0000

    first App

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function Page(){
+    return (
+        <div>
+            <h1>Pagina</h1>
+            <Counter />
+        </div>
+    )
+}
+
+function Counter(){
+    const [count, setCount] = useState(0)
+    function addCounter(){
+        setCount(count + 1)
+    }
+    return (
+        <div>
+            <div>{count}</div>
+            <button onClick={addCounter}>Add</button>
+        </div>
+    )
+}
+
+export default Page
